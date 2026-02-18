701b835ffa7cc1110c6eb2cb957fa22fcea9b652
Ken Barker <kbarker@wavelynx.com>
2026-02-18 14:52:49 -0700

    Add peakmon formula

---
+++ Formula/peakmon.rb
@@ -0,0 +1,15 @@
+class Peakmon < Formula
+  desc "Real-time terminal system monitor for macOS"
+  homepage "https://github.com/kenbarker/peakmon"
+  url "https://github.com/kenbarker/peakmon/releases/download/v#{version}/peakmon-v#{version}-aarch64-apple-darwin.tar.gz"
+  sha256 "10bf42439983b5e2aea7a95854549a45a8701e70265a2f66d635ec4d34be046b"
+  license "MIT"
+
+  def install
+    bin.install "peakmon"
+  end
+
+  test do
+    assert_match "peakmon", shell_output("#{bin}/peakmon --version")
+  end
+end
