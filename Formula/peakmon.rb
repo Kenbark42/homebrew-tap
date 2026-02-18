f02cbdb095f58a626bbe99765eab1dd31259d982
Ken Barker <kbarker@wavelynx.com>
2026-02-18 15:13:04 -0700

    Update peakmon to v0.2.0

--- Formula/peakmon.rb
+++ Formula/peakmon.rb
@@ -1,9 +1,9 @@
 class Peakmon < Formula
   desc "Real-time terminal system monitor for macOS"
   homepage "https://github.com/Kenbark42/peakmon"
-  version "0.1.0"
+  version "0.2.0"
   url "https://github.com/Kenbark42/peakmon/releases/download/v#{version}/peakmon-v#{version}-aarch64-apple-darwin.tar.gz"
-  sha256 "10bf42439983b5e2aea7a95854549a45a8701e70265a2f66d635ec4d34be046b"
+  sha256 "13c6f6c7116b90e547a11e8b1ade16a69d03d547b8f6a38e3ddb251c919fd93b"
   license "MIT"
 
   def install
